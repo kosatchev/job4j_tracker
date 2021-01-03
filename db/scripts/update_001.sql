943ea0611e4be31b7459abd5395a98690ac65501
kosatchev <kosatchev@yandex.com>
2021-01-04 01:27:29 +0500

    4.4. Liquibase. Интеграционные тесты. Proxy. [#289331]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,4 +1,11 @@
-create table items_test (
-   id serial primary key not null,
-   name varchar(2000)
+CREATE sequence items_id_seq;
+CREATE TABLE items (
+	id int4 NOT NULL DEFAULT nextval('items_id_seq'),
+	"name" text NOT NULL,
+	description text NOT NULL,
+	createdate timestamp DEFAULT now(),
+	user_id int4 NOT NULL,
+	category_id int4 NOT NULL,
+	req_state_id int4 NOT NULL,
+	CONSTRAINT item_pk PRIMARY KEY (id)
 );
